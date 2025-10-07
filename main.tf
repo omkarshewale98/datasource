bb22b748d4dbd508c623c21b8fd633353e5c0f64
Omkar Shewale <shewaleomkar1@gmail.com>
2025-10-07 18:49:00 +0530

    i can one more subnet resource

--- main.tf
+++ main.tf
@@ -17,3 +17,10 @@ resource "aws_subnet" "my_subnet" {
     Name = "my-public-subnet"
   }
 }
+# 3️⃣ Create an Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-internet-gateway"
+  }
+}
