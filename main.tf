1abcfc6d80469ac6ba5a7fff27ebe32591735be7
Omkar Shewale <shewaleomkar1@gmail.com>
2025-10-07 18:44:44 +0530

    i can add one vpc and one one subnet code

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# 2️⃣ Create a Subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "my-public-subnet"
+  }
+}
